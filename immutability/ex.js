d4ea7b454fb6a7faf16db1fe0d40e6a44815c593
Angel Ivanov <angel.iwanov@gmail.com>
2024-04-01 21:36:20 +0300

    immutability

--- immutability/ex.js
+++ immutability/ex.js
@@ -4,12 +4,23 @@ function lotteryNum() {
 	return (Math.round(Math.random() * 100) % 58) + 1;
 }
 
-function pickNumber() {}
+function pickNumber(num, numbers) {
+	numbers = numbers.slice();
+	if (!numbers.includes(num)) {
+		numbers = [...numbers, num]
+		numbers.sort((a,b) => a - b)
+	}
+	return numbers;
+}
 
 var luckyLotteryNumbers = [];
+const NUM_CONST = 6;
 
-while (luckyLotteryNumbers.length < 6) {
-	pickNumber();
+while (luckyLotteryNumbers.length < NUM_CONST) {
+	luckyLotteryNumbers = pickNumber(
+		lotteryNum(),
+		Object.freeze(luckyLotteryNumbers)
+	);
 }
 
 console.log(luckyLotteryNumbers);
